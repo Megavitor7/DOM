9ac9f6d5d83bb9dd6a25d9bd4e531a732756ae52
Megavitor7 <117844835+Megavitor7@users.noreply.github.com>
2023-01-12 15:05:45 +0100

    Add files via upload
    lol

---
+++ exo1/main.js
@@ -0,0 +1,17 @@
+let h1 = document.querySelectorAll(`h1`)
+let p = document.getElementsByTagName(`p`)
+let h31 = document.getElementsByTagName(`h3`)[0]
+let h32 = document.getElementsByTagName(`h3`)[1]
+let h33 = document.getElementsByTagName(`h3`)[2]
+
+let p1 = document.getElementsByTagName(`p`)[1] 
+let li = document.querySelector(`li`)
+let li2 = document.getElementsByTagName(`li`)[2] 
+
+
+console.log(h1);
+console.log(h31);
+console.log(h32);
+console.log(h33);
+console.log(p1);
+console.log(li);
